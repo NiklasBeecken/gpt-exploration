f9903242d87b4fa9dd4ae9221542ac82939e2bdf
Niklas Beecken <beeckenniklas@gmail.com>
2023-03-25 12:51:59 +0100

    add notebook

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import os
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-gpt_prompt = "Correct this to standard English:\n\nShe no went to the market."
+gpt_prompt = "Write a hilarious joke about a cat with a hat."
 
 response = openai.Completion.create(
   engine="text-davinci-002",
   prompt=gpt_prompt,
-  temperature=0.5,
+  temperature=1.0,
   max_tokens=256,
   top_p=1.0,
   frequency_penalty=0.0,
